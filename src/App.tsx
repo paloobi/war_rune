5744964363fda5e79f3f7a8a4daf2a3b2fb68d42
Allie Polubiec <alexandra.polubiec@gmail.com>
2023-10-27 15:00:55 -0700

    refactor everything to use react context

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import "./App.css";
 import { GameStage, type GameState } from "./game/types/game.ts";
 import PlayerDebugInfo from "./components/PlayerDebugInfo.tsx";
 import { ACTION_DELAY } from "./game/utils.ts";
-import { GameContext } from "./game/gameContext.ts";
+import { GameContext } from "./game/GameContext.ts";
 import PlayerHand from "./components/PlayerHand.tsx";
 
 function App() {
   const [game, setGame] = useState<GameState>();
   const [playerId, setPlayerId] = useState<string>();
+  const gameContext = useMemo(
+    () => ({
+      game: game || null,
+      player:
+        (game &&
+          Object.values(game.players).find(
+            (player) => player.playerId === playerId
+          )) ||
+        null,
+    }),
+    [game, playerId]
+  );
+
   useEffect(() => {
     Rune.initClient({
       onChange: ({ game, yourPlayerId }) => {
@@ -37,27 +50,13 @@ function App() {
 
   return (
     <>
-      <GameContext.Provider
-        value={{
-          game,
-          player:
-            playerId === game.players.one.playerId
-              ? game.players.one
-              : game.players.two,
-        }}
-      >
+      <GameContext.Provider value={gameContext}>
         <h1>War</h1>
         {game.stage === GameStage.Start && (
           <button onClick={onDeal}>Deal Cards</button>
         )}
         <div>
-          <PlayerHand />
-          {playerId === game.players.one.playerId && (
-            <PlayerDebugInfo game={game} playerNumber="one" />
-          )}
-          {playerId === game.players.two.playerId && (
-            <PlayerDebugInfo game={game} playerNumber="two" />
-          )}
+          <PlayerDebugInfo />
         </div>
       </GameContext.Provider>
     </>
