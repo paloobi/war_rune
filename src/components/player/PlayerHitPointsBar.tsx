20b30581512ff3ecbe98a5ab057bbabe2e0b4326
Allie Polubiec <alexandra.polubiec@gmail.com>
2023-10-28 16:34:26 -0700

    load the HP bar in the Player Panel

--- src/components/player/PlayerHitPointsBar.tsx
+++ src/components/player/PlayerHitPointsBar.tsx
@@ -1,12 +1,13 @@
 import { useContext } from "react";
 import { GameContext } from "../../game/GameContext";
+import HitPointsBar from "../common/HitPointsBar";
 
 const PlayerHitPointsBar = () => {
   const { player } = useContext(GameContext);
   if (!player) {
     throw new Error("cannot render hit bar before game start");
   }
-  return <div>{player.hp}</div>;
+  return <HitPointsBar hp={player.hp} label={"player"} />;
 };
 
 export default PlayerHitPointsBar;
