d0c271bd11f47da3890716d149fc968aa680d41a
Allie Polubiec <alexandra.polubiec@gmail.com>
2023-10-27 21:58:18 -0700

    start aligning player panel in a grid

--- src/components/player/PlayerPanel.tsx
+++ src/components/player/PlayerPanel.tsx
@@ -1,14 +1,19 @@
 import PlayerDeck from "./PlayerDeck";
 import PlayerHand from "./PlayerHand";
 import PlayerHitPointsBar from "./PlayerHitPointsBar";
+import "./PlayerPanel.css";
 
 const PlayerPanel = () => {
   return (
-    <>
-      <PlayerHitPointsBar />
-      <PlayerDeck />
-      <PlayerHand />
-    </>
+    <div className="playerPanel">
+      <div className="playerPanel_left">
+        <PlayerHand />
+        <PlayerHitPointsBar />
+      </div>
+      <div className="playerPanel_deck">
+        <PlayerDeck />
+      </div>
+    </div>
   );
 };
 
