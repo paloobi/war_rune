73fc257069aadef47b08cc73eee9842a0d3e9021
Allie Polubiec <alexandra.polubiec@gmail.com>
2023-10-28 17:01:44 -0700

    style the hp bar

--- src/components/common/HitPointsBar.tsx
+++ src/components/common/HitPointsBar.tsx
@@ -2,13 +2,29 @@ import { MAX_HP } from "../../logic";
 import "./HitPointsBar.css";
 
 const HitPointsBar = ({ hp, label }: { hp: number; label: string }) => {
+  console.log(hp);
+  const getClassName = (): string => {
+    if (hp < MAX_HP * 0.2) {
+      return "hp_bar-low";
+    }
+    if (hp < MAX_HP / 2) {
+      return "hp_bar-mid";
+    }
+    return "hp_bar-normal";
+  };
+
   return (
-    <>
-      <label htmlFor={`hp_bar-${label}`}>HP</label>
-      <progress id={`hp_bar-${label}`} max={MAX_HP} value={hp}>
-        70%
-      </progress>
-    </>
+    <div className="hp_bar-container">
+      <label className="hp_bar-label" htmlFor={`hp_bar-${label}`}>
+        hp
+      </label>
+      <progress
+        id={`hp_bar-${label}`}
+        className={`hp_bar ${getClassName()}`}
+        max={MAX_HP}
+        value={hp}
+      />
+    </div>
   );
 };
 
